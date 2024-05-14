cff27f9ffb2f5755d171a58473d1949d80efe017
Uzaif Ahmad <suzaifahmad@gmail.com>
2024-05-14 16:06:24 +0530

    Initial Commit

---
+++ EQTCommerce/src/main/java/com/eqt/EqtCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.eqt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqtCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqtCommerceApplication.class, args);
+	}
+
+}
